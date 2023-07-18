8dc51ff763a43ccc5b9f09ca73374312000b0b0b
mohamed taha93 <mohamedtahadawoud@gmail.com>
2023-07-18 13:44:29 +0300

    login in

--- app/src/main/java/com/mohamednader/shoponthego/Auth/Login/View/LoginActivity.kt
+++ app/src/main/java/com/mohamednader/shoponthego/Auth/Login/View/LoginActivity.kt
@@ -1,12 +1,145 @@
 package com.mohamednader.shoponthego.Auth.Login.View
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.ProgressDialog
+import android.content.DialogInterface
+import android.content.Intent
 import android.os.Bundle
-import com.mohamednader.shoponthego.R
+import android.text.InputType
+import android.text.TextUtils
+import android.widget.EditText
+import android.widget.LinearLayout
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.tasks.Task
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import com.mohamednader.shoponthego.Auth.SignUp.View.SignUpActivity
+import com.mohamednader.shoponthego.databinding.ActivityLoginBinding
+import java.util.*
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var firebaseAuth: FirebaseAuth
+  private lateinit var loadingBar :ProgressDialog
+    private lateinit var progressDialog :ProgressDialog
+    private lateinit var binding: ActivityLoginBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(getLayoutInflater())
+        setContentView(binding.getRoot())
+        firebaseAuth = Firebase.auth
+        progressDialog = ProgressDialog(this)
+        val preferences = getSharedPreferences("user", MODE_PRIVATE)
+        binding.login.setOnClickListener {
+            userLogin(
+                binding.emailEditText.getText().toString().trim { it <= ' ' },
+                binding.passwordEditText.getText().toString().trim { it <= ' ' })
+        }
+        binding.forgetpassword.setOnClickListener {
+            showRecoverPasswordDialog()
+
+        }
+        binding.createnewAccout.setOnClickListener {
+            startActivity(Intent(this@LoginActivity, SignUpActivity::class.java))
+
+        }
+
+
+    }
+
+    private fun showRecoverPasswordDialog() {
+        val linearLayout = LinearLayout(this)
+        val emailEditText = EditText(this)
+        emailEditText.hint = "E-mail"
+        emailEditText.minEms = 10
+        emailEditText.inputType = InputType.TYPE_TEXT_VARIATION_EMAIL_ADDRESS
+        linearLayout.addView(emailEditText)
+        linearLayout.setPadding(10, 10, 10, 10)
+        MaterialAlertDialogBuilder(this, androidx.transition.R.style.AlertDialog_AppCompat)
+            .setTitle("change Password")
+            .setMessage("After confirmation, you will receive an email with a link to change your password")
+            .setView(linearLayout)
+            .setPositiveButton("Confrimation") { dialogInterface: DialogInterface?, i: Int ->
+                val email = emailEditText.text.toString().trim { it <= ' ' }
+                val emailPattern :Regex = Regex("[a-zA-Z0-9._-]+@[a-z]+\\.[a-z]+")
+                if (TextUtils.isEmpty(email)) {
+                    Toast.makeText(this, "Please enter email", Toast.LENGTH_SHORT).show()
+                    return@setPositiveButton
+                }
+                if (!email.matches(emailPattern)) {
+                    Toast.makeText(this, "Please enter a valid email", Toast.LENGTH_SHORT).show()
+                    return@setPositiveButton
+                }
+                beginRecovery(email)
+            }
+            .setNeutralButton(
+                "cancel"
+            ) { dialogInterface: DialogInterface, i: Int -> dialogInterface.dismiss() }
+            .show()
+    }
+    private fun userLogin(email: String, password: String) {
+        val emailPattern = Regex("[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+")
+        if (TextUtils.isEmpty(email)) {
+            Toast.makeText(this, "Please enter email", Toast.LENGTH_SHORT).show()
+            return
+        }
+        if (!email.matches(emailPattern)) {
+            Toast.makeText(this, "Please enter a valid email", Toast.LENGTH_SHORT).show()
+            return
+        }
+        if (TextUtils.isEmpty(password)) {
+            Toast.makeText(this, "Please enter your password", Toast.LENGTH_SHORT).show()
+            return
+        }
+        progressDialog.setMessage("Signing in...")
+        progressDialog.show()
+        firebaseAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(
+            this
+        ) { task: Task<AuthResult?> ->
+            if (task.isSuccessful) {
+                val preferences =
+                    getSharedPreferences("user", MODE_PRIVATE)
+                preferences.edit()
+                    .putBoolean("flag", true).apply()
+//                startActivity(Intent(this, MainActivity::class.java))
+                Toast.makeText(this@LoginActivity, "Logged in successfully", Toast.LENGTH_SHORT).show()
+            } else {
+//                Toast.makeText(LoginActivity.this, "Wrong email or password, or an error with the Internet connection", Toast.LENGTH_SHORT).show();
+                Toast.makeText(
+                    this@LoginActivity,
+                    Objects.requireNonNull(task.exception)
+                        !!.localizedMessage,
+                    Toast.LENGTH_LONG
+                ).show()
+                progressDialog.dismiss()
+            }
+        }
+    }
+
+    private fun beginRecovery(email: String) {
+        loadingBar = ProgressDialog(this)
+        loadingBar.setMessage("Confirmation message is being sent")
+        loadingBar.setCanceledOnTouchOutside(false)
+        loadingBar.show()
+        firebaseAuth.sendPasswordResetEmail(email).addOnCompleteListener { task ->
+            loadingBar.dismiss()
+            if (task.isSuccessful()) {
+                Toast.makeText(
+                    this@LoginActivity,
+                    "Confirmation message sent successfully, check your email",
+                    Toast.LENGTH_LONG
+                ).show()
+            } else {
+                Toast.makeText(
+                    this@LoginActivity,
+                    "Failed to send confirmation message, this email is not registered",
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+        }.addOnFailureListener { e -> loadingBar.dismiss() }
     }
 }
