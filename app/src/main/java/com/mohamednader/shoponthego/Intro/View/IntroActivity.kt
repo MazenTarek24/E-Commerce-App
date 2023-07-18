fdfc9d5c10383d683f96d8099075314b8ed73984
mazen tarek <94558164+MazenTarek24@users.noreply.github.com>
2023-07-18 12:42:21 +0300

    navigation-ui home screen-ui

--- app/src/main/java/com/mohamednader/shoponthego/Intro/View/IntroActivity.kt
+++ app/src/main/java/com/mohamednader/shoponthego/Intro/View/IntroActivity.kt
@@ -8,5 +8,6 @@ class IntroActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_intro)
+
     }
 }
--- app/src/main/java/com/mohamednader/shoponthego/MainHome/View/MainHomeActivity.kt
+++ app/src/main/java/com/mohamednader/shoponthego/MainHome/View/MainHomeActivity.kt
@@ -2,11 +2,31 @@ package com.mohamednader.shoponthego.MainHome.View
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationItemView
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.mohamednader.shoponthego.R
+import com.mohamednader.shoponthego.databinding.ActivityMainHomeBinding
+
 
 class MainHomeActivity : AppCompatActivity() {
+
+    lateinit var navController: NavController
+    lateinit var binding: ActivityMainHomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main_home)
+        binding = ActivityMainHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment =supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+        val bottomNavView =findViewById<BottomNavigationView>(R.id.bottom_navigation)
+
+        setupWithNavController(bottomNavView, navController)
+
     }
 }
+
