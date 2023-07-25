9ca1260bacd1678089d1b85d384ee273980a6f10
mazen tarek <94558164+MazenTarek24@users.noreply.github.com>
2023-07-25 17:34:24 +0300

    commit and upload some missing files

---
+++ app/src/main/java/com/mohamednader/shoponthego/Categories/View/PriceFilterDialogFragment.kt
@@ -0,0 +1,98 @@
+package com.mohamednader.shoponthego.Categories.View
+
+import android.app.Dialog
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.SeekBar
+
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+import com.mohamednader.shoponthego.databinding.SliderItemBinding
+
+
+class PriceFilterDialogFragment : BottomSheetDialogFragment() {
+
+    interface PriceFilterListener{
+        fun onPriceFiltered(priceFrom : Double , priceTo : Double)
+    }
+
+    companion object {
+        fun newInstance(): PriceFilterDialogFragment {
+            return PriceFilterDialogFragment()
+        }
+    }
+
+    private var _binding: SliderItemBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+        // Inflate the layout for this fragment
+        _binding = SliderItemBinding.inflate(inflater , container , false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.sbPriceFrom.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                val priceFrom = progress.toDouble()
+                binding.etPriceFrom.setText(priceFrom.toString())
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+            }
+        })
+
+        binding.sbPriceTo.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                val priceTo = progress.toDouble()
+                binding.etPriceTo.setText(priceTo.toString())
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+            }
+        })
+
+        binding.btnApply.setOnClickListener {
+            val priceFrom = binding.etPriceFrom.text.toString().toDoubleOrNull() ?: 0.0
+            val priceTo = binding.etPriceTo.text.toString().toDoubleOrNull() ?: Double.MAX_VALUE
+
+            val listener = targetFragment
+            if (listener is PriceFilterListener) {
+                listener.onPriceFiltered(priceFrom, priceTo)
+            }
+
+            dismiss()
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        dialog?.window?.setLayout(
+            ViewGroup.LayoutParams.MATCH_PARENT,
+            ViewGroup.LayoutParams.WRAP_CONTENT,
+        )
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+}
---
+++ app/src/main/java/com/mohamednader/shoponthego/Categories/ViewModel/CategoryAdapter.kt
@@ -0,0 +1,53 @@
+package com.mohamednader.shoponthego.BranProduct.view
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.mohamednader.shoponthego.Model.Pojo.Products.Product
+import com.mohamednader.shoponthego.databinding.ItemProductBinding
+import com.squareup.picasso.Picasso
+import java.text.DecimalFormat
+
+class CategoryAdapter : ListAdapter<Product , CategoryAdapter.ViewHolder>(BrandDiffUtil()) {
+
+    public class ViewHolder(val binding : ItemProductBinding) : RecyclerView.ViewHolder(binding.root){
+
+    }
+    private var productsBrand: List<Product> = listOf()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(ItemProductBinding.inflate(LayoutInflater.from(parent.context) ,
+            parent , false ))
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val product = getItem(position)
+        if (product != null) {
+            Picasso.get().load(product.image.src).into(holder.binding.itemImg)
+            holder.binding.titleProduct.text = product.title
+
+            var price = product.variants?.getOrNull(0)?.price ?: 1.0
+            holder.binding.priceProduct.text = price.toString() + " EGP "
+
+
+//            holder.binding.priceProduct.text = product.vendor
+        }
+    }
+
+    fun deleteProductBrand(){
+        productsBrand = listOf()
+        notifyDataSetChanged()
+    }
+
+    class BrandDiffUtil : DiffUtil.ItemCallback<Product>() {
+        override fun areItemsTheSame(oldItem: Product, newItem: Product): Boolean {
+            return oldItem === newItem
+        }
+
+        override fun areContentsTheSame(oldItem: Product, newItem: Product): Boolean {
+            return oldItem == newItem
+        }
+    }
+}
